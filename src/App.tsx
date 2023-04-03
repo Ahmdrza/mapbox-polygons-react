f946b6239291b16e640ceff4ac0f29655b85c12b
Ahmad Raza <ahmdrzalifa@gmail.com>
2023-04-03 17:46:33 +0100

    add polygons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 
 import './App.css';
-import { Map } from 'react-map-gl';
+import { GeoJSONSourceOptions } from 'mapbox-gl';
+import { Layer, Map, Source } from 'react-map-gl';
 
 function App() {
+
+  const polygons: GeoJSONSourceOptions['data'] = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        id: 1,
+        type: 'Feature',
+        properties: {
+          name: 'Area 51',
+        },
+        geometry: {
+          type: 'MultiPolygon',
+          coordinates: [
+            [
+              [
+                [-7.950966710379959, 53.43146017152239],
+                [-7.950966710379959, 53.42962140120139],
+                [-7.947024882898944, 53.42962140120139],
+                [-7.947024882898944, 53.43146017152239],
+                [-7.950966710379959, 53.43146017152239],
+              ],
+            ],
+          ],
+        },
+      },
+    ],
+  };
+
   return (
     <div style={{height: '100vh', width: '100%'}}>
       <Map
@@ -17,7 +46,30 @@ function App() {
           latitude: 53.4313602035036,
           zoom: 15
         }}
-       />
+       >
+        <Source id='source' type='geojson' data={polygons}>
+          <Layer
+            {...{
+              id: 'data',
+              type: 'fill',
+              paint: {
+                'fill-color': 'rgb(5, 191, 5)',
+                'fill-opacity': 0.4
+              },
+            }}
+          />
+          <Layer
+            {...{
+              id: 'outline',
+              type: 'line',
+              paint: {
+                'line-color': 'red',
+                'line-width': 2,
+              },
+            }}
+          />
+          </Source>
+        </Map>
     </div>
   );
 }
